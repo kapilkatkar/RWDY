9b3c5435c0b455d4e90fa1643cb5af464d1285db
kapilkatkar <kapilkatkar32@gamil.com>
2025-02-19 23:56:41 +0530

    added cards to the home

--- RWDY/src/components/Home.jsx
+++ RWDY/src/components/Home.jsx
@@ -8,8 +8,31 @@ const Home = () => {
         <h1 className="site-title">RWDY</h1>
       </header>
       <Headers />
+      <section className="products">
+        <div className="product">
+          <img src="/img1.jpg" alt="Product 1" />
+          <h3>THE RWDY DENIM OVERSHIRT</h3>
+        </div>
+        <div className="product">
+          <img src="/img2.jpg" alt="Product 2" />
+          <h3>RWDY ESSENTIAL HOODIE </h3>
+        </div>
+        <div className="product">
+          <img src="/img3.jpg" alt="Product 3" />
+          <h3>CULTURE BLACK HOODIE</h3>
+        </div>
+        <div className="product">
+          <img src="/img4.jpg" alt="Product 3" />
+          <h3> RWDY SWEATSHIRT</h3>
+        </div>
+      </section>
+
       <div>
-        <img src="/exclusiveCol.jpg" alt="okkkkk" />
+        <img
+          src="/exclusiveCol.jpg"
+          alt="okkkkk"
+          className="exclusiveCol-img"
+        />
       </div>
     </div>
   );
